a92b1a0be4df172451ffbb6f639720756bc14ff3
Edwin <ohwofasaedwin@gmail.com>
2024-10-27 20:48:23 +0100

    first commit

---
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.layouts
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+//     lateinit var Jane: TextView
+//     lateinit var status_bar: ImageView
+//    lateinit var menu: ImageView
+//    lateinit var GoodMorning: TextView
+//    lateinit var love_symbol: ImageView
+//    lateinit var search_bar: Button
+//    lateinit var
+//    lateinit var
+//    lateinit var
+//    lateinit var
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+
+        }
+    }
+
